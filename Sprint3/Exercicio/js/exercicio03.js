e06800c51fe8a344e2f46cd873a5705010ada278
joao rossi <157733212+joaozinhopro2709@users.noreply.github.com>
2024-06-13 15:52:29 -0300

    13/06

---
+++ Sprint3/Exercicio/js/exercicio03.js
@@ -0,0 +1,3 @@
+var nome = String(prompt('Qual seu nome: '))
+var idade = Number(prompt('Qual sua idade: '))
+document.write('Olá '+ nome +', tudo bem? sua idade é '+ idade +' anos, certo?')
--- Sprint3/Exercicio/js/teste.js
+++
@@ -1,3 +0,0 @@
-var nome = String(prompt('Qual seu nome: '))
-var idade = Number(prompt('Qual sua idade: '))
-document.write('Olá '+ nome +', tudo bem? sua idade é '+ idade +' anos, certo?')
